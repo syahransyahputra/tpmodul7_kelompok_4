21ac77f581d6ff958b5197768b88baa4bd22af71
muhammadzulfadly <muhammadzulfadly@student.telkomuniversity.ac.id>
2023-04-02 22:15:16 +0700

    menambahkan class DataMahasiswa1302210046

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace tpmodul7_kelompok_4
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            DataMahasiswa1302210046 dataMHS = new DataMahasiswa1302210046();
+            dataMHS.ReadJSON();
+        }
+    }
+}
