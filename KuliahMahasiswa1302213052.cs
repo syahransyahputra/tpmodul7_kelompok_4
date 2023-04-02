b8dc29d3d97c5a3efb5038e4ff458fde61ec5748
muhammadzulfadly <muhammadzulfadly@student.telkomuniversity.ac.id>
2023-04-02 22:33:00 +0700

    menambahkan class KuliahMahasiswa1302213052

---
+++ KuliahMahasiswa1302213052.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.Json;
+
+namespace tpmodul7_kelompok_4
+{
+    public class KuliahMahasiswa1302213052
+    {
+        public void ReadJSON()
+        {
+            String jsonString = File.ReadAllText("/Users/muhammadzulfadly/Documents/GitHub/tpmodul7_kelompok_4/tp7_2_1302213052.json");
+
+            Kuliah course = JsonSerializer.Deserialize<Kuliah>(jsonString);
+
+            Console.WriteLine("Daftar mata kuliah yang diambil:");
+
+            int i = 1;
+
+            foreach (Course courses in course.courses)
+            {
+                Console.WriteLine("MK {0:d} {1:s} - {2:s}", i, courses.code, courses.name);
+                i++;
+            }
+
+        }
+    }
+
+
+    public class Kuliah
+    {
+        public List<Course> courses { get; set; }
+
+        public Kuliah() { }
+
+        public Kuliah(List<Course> courses)
+        {
+            this.courses = courses;
+        }
+    }
+
+    public class Course
+    {
+        public string code { get; set; }
+
+        public string name { get; set; }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace tpmodul7_kelompok_4
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            KuliahMahasiswa1302213052 kuliah = new KuliahMahasiswa1302213052();
+            kuliah.ReadJSON();
+        }
+
+    }
+}
